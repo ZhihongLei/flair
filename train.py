294fa66701a7feff4d8d4d987ed28fd077a6ab4d
Zhihong Lei <zhihonglei@hotmail.com>
2019-03-25 00:14:48 +0100

    debug

--- train.py
+++ train.py
@@ -6,12 +6,6 @@ import torch
 from torch.optim.adam import Adam
 from torch.optim.sgd import SGD
 
-torch.manual_seed(0)
-torch.backends.cudnn.deterministic = True
-torch.backends.cudnn.benchmark = False
-import numpy as np
-np.random.seed(0)
-
 
 def pooled_embeddings(s):
     try:
