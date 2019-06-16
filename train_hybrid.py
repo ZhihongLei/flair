f44e36a6e2f4cf381f357bb6b33439d55cfbc41c
Zhihong Lei <zhihonglei@hotmail.com>
2019-06-16 18:59:17 +0200

    debug

--- train_hybrid.py
+++ train_hybrid.py
@@ -14,7 +14,11 @@ from flair.models.sequence_tagger_model import HybridSequenceTagger
 from flair.models.language_model import MyLanguageModel, MySimpleLanguageModel
 from flair.models import SequenceTagger
 from flair.trainers import ModelTrainer
+from flair.trainers.language_model_trainer import MySimpleLMTrainer
+import logging
 
+
+log = logging.getLogger('flair')
 
 def def_pooled_embeddings(s):
     try:
@@ -50,6 +54,8 @@ parser.add_argument('--tag-type', required=True, help='Tag type to train')
 parser.add_argument('--beam-size', type=int, default=-1, help='Beam size')
 parser.add_argument('--lm-weight', type=float, default=1.0, help='Weight of language model score')
 parser.add_argument('--word-embeddings', nargs='*', help='Type(s) of word embeddings')
+parser.add_argument('--init-lm', help='Initial language model')
+parser.add_argument('--init-tagger', help='Initial tagger model')
 parser.add_argument('--char-embeddings', action='store_true', help='Character embeddings trained on task corpus, Lample 2016')
 parser.add_argument('--flair-embeddings', nargs='*', help='Type(s) of Flair embeddings')
 parser.add_argument('--relearn-embeddings', action='store_true', help='Re-learn embeddings, might be useful when using pretrained embeddings')
@@ -62,98 +68,123 @@ parser.add_argument('--dropout-rate', type=float, default=0.0, help='Dropout rat
 parser.add_argument('--no-crf', action='store_true', help='Do not use CRF')
 parser.add_argument('--optimizer', default='sgd', choices=['sgd', 'adam'], help='Type of optimizer')
 parser.add_argument('--init-lr', type=float, default=0.1, help='Initial learning rate')
+parser.add_argument('--pretraining-epochs', type=int, default=-1, help='Number of pre-training epochs')
 parser.add_argument('--num-epochs', type=int, default=20, help='Number of epochs')
 parser.add_argument('--working-dir', default='.', help='Working directory where outputs are stored')
 
 
 args = parser.parse_args()
-print("CUDA_VISIBLE_DEVICES={}".format(os.environ.get("CUDA_VISIBLE_DEVICES")))
-
+log.info("CUDA_VISIBLE_DEVICES={}".format(os.environ.get("CUDA_VISIBLE_DEVICES")))
 
 task, path = args.task
 embeddings_in_memory = True if task == 'conll_03' else False
-print('Task {}'.format(task))
+log.info('Task {}'.format(task))
 
 corpus: TaggedCorpus = NLPTaskDataFetcher.load_corpus(task, path)
-print(corpus)
+log.info(corpus)
 tag_type = args.tag_type
-print('Corpus has been read')
-
-
-
-
-
-# initialize sequence tagger
-
 
 
 if os.path.isdir(args.working_dir) and os.path.isfile(os.path.join(args.working_dir, 'best-model.pt')):
-    print('Loading initial model from ' + os.path.join(args.working_dir, 'best-model.pt'))
+    log.info('Loading initial model from ' + os.path.join(args.working_dir, 'best-model.pt'))
     model = HybridSequenceTagger.load_from_file(os.path.join(args.working_dir, 'best-model.pt'))
 else:
-    print('Initializing model ...')
+    log.info('Initializing model ...')
 
     # initialize embeddings
-    embedding_types: List[TokenEmbeddings] = [WordEmbeddings(t) if t != 'task-trained' \
-                                                  else NonStaticWordEmbeddings(100,
-                                                                               corpus.make_vocab_dictionary(min_freq=2)) \
-                                              for t in args.word_embeddings]
-
-    if args.char_embeddings:
-        embedding_types.append(CharacterEmbeddings())
-    if args.flair_embeddings:
-        embedding_types.extend([FlairEmbeddings(type) for type in args.flair_embeddings])
-    if args.pooled_flair_embeddings:
-        embedding_types.extend([PooledFlairEmbeddings(type, pooling) for type, pooling in args.pooled_flair_embeddings])
-
-    if len(embedding_types) == 0:
-        raise ValueError('Must specify at least one embedding type!')
-
-    embeddings: StackedEmbeddings = StackedEmbeddings(embeddings=embedding_types)
-
     tag_dictionary = corpus.make_tag_dictionary(tag_type=tag_type)
-    print(tag_dictionary.idx2item)
 
-    additional_tag_dictionaries = []
-    additional_tag_embeddings = []
-    if args.additional_embeddings:
-        for tag, size in args.additional_embeddings:
-            d = corpus.make_tag_dictionary(tag_type=tag)
-            additional_tag_dictionaries.append(d)
-            additional_tag_embeddings.append(NonStaticWordEmbeddings(size, d, tag, args.window_size))
-        additional_tag_embeddings = torch.nn.ModuleList(additional_tag_embeddings)
-        for d in additional_tag_dictionaries: print(d.idx2item)
+
+    if args.init_lm:
+        log.info(f'Loading initial language model from {args.init_lm}')
+        lm = MySimpleLanguageModel.load_from_file(args.init_lm)
+    else:
+        log.info('Initializing language model ...')
+        lm = MySimpleLanguageModel(tag_type=tag_type,
+                                   embedding_size=10,
+                                   dictionary=tag_dictionary,
+                                   hidden_size=50,
+                                   nlayers=1)
+    if args.init_tagger:
+        log.info(f'Loading initial tagger model from {args.init_tagger}')
+        tagger = SequenceTagger.load_from_file(args.init_tagger)
+    else:
+        additional_tag_dictionaries = []
+        additional_tag_embeddings = []
+        if args.additional_embeddings:
+            for tag, size in args.additional_embeddings:
+                d = corpus.make_tag_dictionary(tag_type=tag)
+                additional_tag_dictionaries.append(d)
+                additional_tag_embeddings.append(NonStaticWordEmbeddings(size, d, tag, args.window_size))
+            additional_tag_embeddings = torch.nn.ModuleList(additional_tag_embeddings)
+            for d in additional_tag_dictionaries: print(d.idx2item)
+
+        embedding_types: List[TokenEmbeddings] = [WordEmbeddings(t) if t != 'task-trained' \
+                                                      else NonStaticWordEmbeddings(100,
+                                                                                   corpus.make_vocab_dictionary(
+                                                                                       min_freq=2)) \
+                                                  for t in args.word_embeddings]
+
+        if args.char_embeddings:
+            embedding_types.append(CharacterEmbeddings())
+        if args.flair_embeddings:
+            embedding_types.extend([FlairEmbeddings(type) for type in args.flair_embeddings])
+        if args.pooled_flair_embeddings:
+            embedding_types.extend(
+                [PooledFlairEmbeddings(type, pooling) for type, pooling in args.pooled_flair_embeddings])
+
+        if len(embedding_types) == 0:
+            raise ValueError('Must specify at least one embedding type!')
+
+        embeddings: StackedEmbeddings = StackedEmbeddings(embeddings=embedding_types)
+
+
+
+        log.info('Initializing tagger model ...')
+        log.info('Using word embeddings: {}'.format(str(embedding_types)))
+        log.info('Using additional tag embeddings: {}'.format(str(additional_tag_embeddings)))
+        log.info('Re-learning embeddings: {}'.format(args.relearn_embeddings))
+        log.info('{} hidden layers of size {}'.format(args.num_hidden_layers, args.hidden_size))
+        log.info('Dropout rate: {}'.format(args.dropout_rate))
+        log.info('Using CRF: {}'.format(not args.no_crf))
+        tagger = SequenceTagger(hidden_size=args.hidden_size,
+                                embeddings=embeddings,
+                                tag_dictionary=tag_dictionary,
+                                additional_tag_embeddings=additional_tag_embeddings,
+                                additional_tag_dictionaries=additional_tag_dictionaries,
+                                tag_type=tag_type,
+                                use_crf=not args.no_crf,
+                                rnn_layers=args.num_hidden_layers,
+                                rnn_dropout=args.dropout_rate,
+                                relearn_embeddings=args.relearn_embeddings)
+
+    if args.pretraining_epochs > 0:
+        if args.init_tagger is None:
+            tagger_pretrainer = ModelTrainer(tagger, corpus, SGD)
+            log.info('-' * 100)
+            log.info(f'Pre-training tagger for {args.pretraining_epochs} epochs')
+            tagger_pretrainer.train(os.path.join(args.working_dir, 'pretraining/tagger'), EvaluationMetric.MICRO_F1_SCORE, learning_rate=args.init_lr, mini_batch_size=32,
+                      max_epochs=args.pretraining_epochs, anneal_factor=0.5, embeddings_in_memory=True,
+                      anneal_against_train_loss=False)
+
+        if args.init_lm is None:
+            train_data, dev_data, test_data = [lm.get_word_indices(data) for data in
+                                               [corpus.train, corpus.dev, corpus.test]]
+            lm_pretrainer = MySimpleLMTrainer(lm, train_data, dev_data, test_data, SGD)
+            log.info(f'Pre-training LM for {args.pretraining_epochs} epochs')
+            lm_pretrainer.train(base_path=os.path.join(args.working_dir, 'pretraining/lm'), learning_rate=2., mini_batch_size=16, max_epochs=args.pretraining_epochs,
+                      anneal_factor=0.5, anneal_against_train_loss=False)
+
+        if args.init_tagger is None or args.init_lm is None:
+            log.info('Pre-training done.')
+            log.info('-'*100)
+
 
     beam_size = len(tag_dictionary.item2idx) if args.beam_size == -1 else args.beam_size
     lm_weight = args.lm_weight
 
-    print('Using word embeddings: {}'.format(str(embedding_types)))
-    print('Using additional tag embeddings: {}'.format(str(additional_tag_embeddings)))
-    print('Re-learning embeddings: {}'.format(args.relearn_embeddings))
-    print('{} hidden layers of size {}'.format(args.num_hidden_layers, args.hidden_size))
-    print('Dropout rate: {}'.format(args.dropout_rate))
-    print('Using CRF: {}'.format(not args.no_crf))
-    print(f'Beam size: {beam_size}')
-    print(f'LM weight: {lm_weight}')
-
-    lm = MySimpleLanguageModel(tag_type=tag_type,
-                        embedding_size=10,
-                        dictionary=tag_dictionary,
-                        hidden_size=50,
-                        nlayers=1)
-
-    tagger = SequenceTagger(hidden_size=args.hidden_size,
-                            embeddings=embeddings,
-                            tag_dictionary=tag_dictionary,
-                            additional_tag_embeddings=additional_tag_embeddings,
-                            additional_tag_dictionaries=additional_tag_dictionaries,
-                            tag_type=tag_type,
-                            use_crf=not args.no_crf,
-                            rnn_layers=args.num_hidden_layers,
-                            rnn_dropout=args.dropout_rate,
-                            relearn_embeddings=args.relearn_embeddings)
-
-
+    log.info(f'Beam size: {beam_size}')
+    log.info(f'LM weight: {lm_weight}')
     model = HybridSequenceTagger(tagger, lm, beam_size, lm_weight)
 
 
@@ -165,16 +196,15 @@ elif args.optimizer == 'adam':
     anneal_factor = 0.999
 else:
     raise ValueError('Cannot recognize optimizer {}'.format(args.optimizer))
-print('Using {}'.format(args.optimizer))
-print('Initial learning rate: {}'.format(args.init_lr))
+log.info('Using {}'.format(args.optimizer))
+log.info('Initial learning rate: {}'.format(args.init_lr))
 
-print(model.parameters)
-#print(model.state_dict)
+log.info(model.parameters)
 
 trainer: ModelTrainer = ModelTrainer(model, corpus, optimizer)
 
 trainer.train(args.working_dir, EvaluationMetric.MICRO_F1_SCORE, learning_rate=args.init_lr, mini_batch_size=32,
-              max_epochs=args.num_epochs, anneal_factor=anneal_factor, embeddings_in_memory=True)
+              max_epochs=args.num_epochs, anneal_factor=anneal_factor, embeddings_in_memory=True, anneal_against_train_loss=False)
 
 plotter = Plotter()
 plotter.plot_training_curves('{}/loss.tsv'.format(args.working_dir))
